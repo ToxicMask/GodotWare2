8b188cef2e13a775ca13ed6a74fe3ad327311ef3
ToxicMask <tomas.carciofi@gmail.com>
2019-04-05 23:27:20 -0300

    Added joints, spawn targets and modify check()

--- Minigames/Godobot/Scripts/Godobot.gd
+++ Minigames/Godobot/Scripts/Godobot.gd
@@ -2,14 +2,27 @@ extends Node2D
 
 signal clear
 
+onready var target = preload("res://Minigames/Godobot/Scenes/Target.tscn")
+
 func _ready():
+	_build_body()
 	self.connect("clear", get_parent(), "win")
 
+func _build_body():
+	
+	var dif = 1
+	
+	for j in $Joints.get_children():
+		var t = target.instance(PackedScene.GEN_EDIT_STATE_INSTANCE)
+		j.add_child(t)
 
 func _check():
+	print("CHECKED")
+	
 	var count: int = 0
-	for T in get_tree().get_nodes_in_group("target"):
-		count +=1
+	
+	for j in $Joints.get_children():
+		count += j.get_child_count()
 	
 	if count == 0:
 		emit_signal("clear")
--- Minigames/Godobot/Scripts/Target.gd
+++ Minigames/Godobot/Scripts/Target.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 func _ready():
-	self.connect("tree_exited", get_parent(),"_check")
+	self.connect("tree_exited",get_parent().get_parent().get_parent(),"_check")
 
 func destroy():
 	queue_free()
